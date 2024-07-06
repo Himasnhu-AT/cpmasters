ab06c7282daca2546ef831229d186dfa548d37f2
Himanshu-AT <aristech.official@gmail.com>
2024-07-06 23:07:50 +0530

    first commit

---
+++ apps/docs/.vitepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    title: "CpMater - devdocs",
+    description: "DevDocs of CpMasters",
+    themeConfig: {
+        nav: [
+            { text: "Home", link: "/" },
+            { text: "Github", link: "https://github.com/Himasnhu-AT/cpmasters" }
+        ],
+        sidebar: [
+            {
+                text: "Getting Started",
+                items: [
+                    { text: "Home", link: "/" },
+                    { text: "Installation", link: "/installation" },
+                    { text: "TO DO", link: "/todo" }
+                ]
+            }
+        ]
+    }
+};
